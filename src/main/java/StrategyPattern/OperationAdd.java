70c2a40ab70f9cd6fe58efa8aa7a339e2b29234e
liuxl <liuxuleiemail@163.com>
2018-10-08 10:55:45 +0800

    first commit

---
+++ src/main/java/StrategyPattern/OperationAdd.java
@@ -0,0 +1,9 @@
+package StrategyPattern;
+
+public class OperationAdd implements Strategy{
+
+
+    public int doOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/main/java/StrategyPattern/OperationMultiply.java
@@ -0,0 +1,7 @@
+package StrategyPattern;
+
+public class OperationMultiply implements Strategy{
+    public int doOperation(int num1, int num2) {
+        return num1 * num2;
+    }
+}
